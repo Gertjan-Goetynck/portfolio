6d7b79f4d077ab994dd92e55340ca106c15ecf2b
Gertjan <gertjan@goetynck.com>
2019-04-01 15:01:30 +0200

    Cleaned up starter components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { Global, css } from "@emotion/core"
@@ -16,13 +9,11 @@ import "typeface-montserrat"
 import "typeface-lato"
 
 import Header from "./header"
-import "./layout.css"
 
 const globalStyles = css`
   body {
     font-family: "Montserrat", sans-serif;
     font-weight: 400;
-    line-height: 1.7;
   }
   h1 {
     font-family: "lato", sans-serif;
@@ -33,20 +24,8 @@ const Layout = ({ children }) => (
     {/* Set Global styles with Emotion */}
     <Global styles={globalStyles} />
     <Header />
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
+    <div>
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
     </div>
   </>
 )
