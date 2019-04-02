aa9f6a5de91537d5be031a7e69a847a05a52af00
Gertjan <gertjan@goetynck.com>
2019-04-02 15:45:38 +0200

    fixed deploy bug

--- src/components/Hero.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
-
-const Hero = ({ children }) => <div>{children}</div>
-
-export default Hero
