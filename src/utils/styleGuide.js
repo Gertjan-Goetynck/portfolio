ca93fc05560d3725cc4481473a44dfb0935dbb9a
Gertjan <gertjan@goetynck.com>
2019-04-03 11:55:00 +0200

    Added styleguide to utils

---
+++ src/utils/styleGuide.js
@@ -0,0 +1,11 @@
+export const colors = {
+  mainColor: "#00D4D4",
+  white: "#F3F3F3",
+}
+
+export const fonts = {
+  sizes: {
+    base: "62.5%", //Sets size of 1 rem to 10px (10/16)
+    headingPrimary: "6rem",
+  },
+}
